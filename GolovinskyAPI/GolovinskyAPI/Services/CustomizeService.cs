a9f9b0fcb9226d4c2c9da2b0f8e182b1ea9c4b58
dddzzz1994 <dddzzz1994@mail.ru>
2018-12-30 15:17:37 -0800

    no message

--- GolovinskyAPI/GolovinskyAPI/Services/CustomizeService.cs
+++ GolovinskyAPI/GolovinskyAPI/Services/CustomizeService.cs
@@ -7,48 +7,67 @@ namespace GolovinskyAPI.Services
 {
     public class CustomizeService
     {
-        public string date = DateTime.Now.ToString("dd.MM.yyyy");
+        
         public string GetMainImage()
         {
-
-          var directoryFiles = Directory.GetFiles("wwwroot/Images");
-
-            /*
-            string image = Array.Find(directoryFiles, s => s.StartsWith(date));
-              
-            var result = image;
-               */
-
-
-            var random = new Random();
-            int index = random.Next(directoryFiles.Length - 1);
-            string image = directoryFiles[index];
-            var result = image.Substring(image.LastIndexOf('\\') + 1);
-
-
-            return result;
-
+            string prefix = "wwwroot/Images\\";
+            string date = DateTime.Now.ToString("dd.MM.yyyy");
+            string searchresult = prefix + date;
+            var directoryFiles = Directory.GetFiles("wwwroot/Images");
+            string imagedate = Array.Find(directoryFiles, s => s.StartsWith(searchresult));
+            if(directoryFiles.Contains(imagedate))
+            {
+                var result = imagedate;
+                return result;
+            }
+            else
+            {
+                var random = new Random();
+                int index = random.Next(directoryFiles.Length - 1);
+                string image = directoryFiles[index];
+                var result = image.Substring(image.LastIndexOf('\\') + 1);
 
 
+                return result;
+
+            }
+            
+           
+           
+          
 
 
 
-          }
 
-          public string GetMainImageUserAccount()
+
+
+
+        }
+
+        public string GetMainImageUserAccount()
           {
-              var directoryFiles = Directory.GetFiles("wwwroot/AccountImages");
-            /*
-               string image = Array.Find(directoryFiles, s => s.StartsWith(date));
-              
-              var result = image;
-              */
-            var random = new Random();
-               int index = random.Next(directoryFiles.Length - 1);
-               string image = directoryFiles[index];
-               var result = image.Substring(image.LastIndexOf('\\') + 1);
-              
-            return result;
+
+            string prefix = "wwwroot/AccountImages\\";
+            string date = DateTime.Now.ToString("dd.MM.yyyy");
+            string searchresult = prefix + date;
+            var directoryFiles = Directory.GetFiles("wwwroot/AccountImages");
+            string imagedate = Array.Find(directoryFiles, s => s.StartsWith(searchresult));
+            if(directoryFiles.Contains(imagedate))
+            {
+                var result = imagedate;
+                return result;
+            }
+            else
+            {
+                var random = new Random();
+                int index = random.Next(directoryFiles.Length - 1);
+                string image = directoryFiles[index];
+                var result = image.Substring(image.LastIndexOf('\\') + 1);
+
+                return result;
+            }
+           
+               
             
         } 
     }
